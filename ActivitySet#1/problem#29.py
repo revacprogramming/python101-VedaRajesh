1fdf5c5112b62218c8cb7f9de508f183eb5951dc
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-05-23 05:41:26 +0000

    23.may.2022

---
+++ ActivitySet#1/problem#29.py
@@ -0,0 +1,6 @@
+#program to calculate simple interest
+p=int(input("enter the principle in rs\t "))
+t=int(input("enter time in hours\t "))
+r=int(input("enter rate\t"))
+SI=(p*t*r)/100
+print("SI is ",SI)
---
+++ ActivitySet#1/problem#30.py
@@ -0,0 +1,8 @@
+# python program to find area of circle and sphere
+# python program to find area of circle and sphere
+rad=int(input("enter the radius\t"))
+ar=3.14*rad*rad
+arsp=4*3.14*rad*rad
+print("area of circle is :\t",ar)
+print("area of sphere is :\t",arsp)
+        
