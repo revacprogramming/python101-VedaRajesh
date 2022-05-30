8173f01a4da9f789cdba85c43ebc7180dec9f6b8
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-05-30 04:36:40 +0000

    done on 30.may.2022

---
+++ ActivitySet#1/problem#33.py
@@ -0,0 +1,14 @@
+# program to swap two numbers
+a=22
+b=32
+print("numbers before swapping")
+print("a is ",a)
+print("b is ",b)
+temp=0
+temp=a
+a=b
+b=temp
+print("numbers after swapping")
+print("a is",a)
+print("b is ",b)
+
