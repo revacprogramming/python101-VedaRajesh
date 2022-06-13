20380373cc812e43e64668612713db1e476be18a
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-06-13 07:21:55 +0000

    13.june

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,7 +1,15 @@
 # Strings
 # Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
-text = "X-DSPAM-Confidence:    0.8475"
-ffind = text.find(':')
-piece = text[ffind+1:]
-end = float(piece)
-print(end)
+def myfunc():
+    text = "X-DSPAM-Confidence:    0.8475"
+    ffind = text.find(':')
+
+def code():
+    piece = text[ffind+1:]
+    end = float(piece)
+
+
+def main(): 
+    print(end)
+
+main()
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,6 +1,7 @@
 
 # Files
 # Use the file name mbox-short.txt as the file name
+
 fname = input("Enter file name: ")
 fh = open(fname)
 for line in fh:
